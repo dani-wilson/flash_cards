de4698dfd9d203dad042457b6cb19db6f7a6b601
Dani Wilson <danirae.powell@gmail.com>
2023-05-08 17:48:22 -0600

    Add working code to the Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,29 @@
 class Turn
 
-    #attr_reader :guess, :correct? :card, :feedback
+    attr_reader :guess, :card
 
     def initialize(guess, card)
         @guess = guess
         @card = card
+    end
+
+    def guess
+        @guess = gets.chomp
+    end
+
+    def correct?
+        if @guess == card.answer
+            true
+        else
+            false
+        end
+    end
+
+    def feedback
+        if @guess == "Juneau"
+            return "Correct!"
+        else
+            return "Incorrect."
+        end
     end
 end
